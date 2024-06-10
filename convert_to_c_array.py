e9497b047a704da5bf54d36a75696aa27e30c456
h-tye <156721697+h-tye@users.noreply.github.com>
2024-06-10 12:54:45 -0400

    Create convert_to_c_array.py

---
+++ convert_to_c_array.py
@@ -0,0 +1,12 @@
+#convert .tflite to cc file and make neccessary adjustments
+model_all_path = "final_model.tflite"
+
+!apt-get -qq install xxd
+
+!xxd -i {model_all_path} > {model_cc_path}
+
+model_cc_path = "model.cc"
+
+# This is needed to make our model compatible with the infastructure(hello_world directory) already in place
+REPLACE_TEXT = model_all_path.replace('/', '_').replace('.', '_')
+!sed -i 's/'{REPLACE_TEXT}'/g_model/g' {model_cc_path}
